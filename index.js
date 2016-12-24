a083d77f7724892da1e2308f6597c0a780d18e48
crazyrohila <sanjay.rohila@srijan.net>
2016-12-24 11:09:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Init App
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/views/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
